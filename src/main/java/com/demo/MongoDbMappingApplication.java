acf460023c437f534cc4268b0b248e3f34f2d5d6
SharvanSaini <500063736@stu.upes.ac.in>
2020-06-15 09:50:23 +0530

    initial commit

---
+++ src/main/java/com/demo/MongoDbMappingApplication.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbMappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbMappingApplication.class, args);
+	System.out.println("start");
+	}
+
+}
---
+++ src/main/java/com/demo/model/User.java
@@ -0,0 +1,58 @@
+package com.demo.model;
+
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Document(collection = "order_DB")
+public class User {
+	@Id
+	private int id;
+	private String name;
+	private String gender;
+	private List<Product> products;
+	private Address address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", gender=" + gender + ", products=" + products + ", address="
+				+ address + "]";
+	}
+
+	
+}
